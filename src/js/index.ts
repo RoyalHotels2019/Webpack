67f4f9b8b1b00e7421c360a76e2583c852eb699c
thom20m1 <45196128+thom20m1@users.noreply.github.com>
2019-12-10 14:00:33 +0100

    Implementation af axios v1

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,14 +3,21 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 
-let uri: string = "localhost:50182/api/hoteltemps";
+let uri: string = "http://localhost:50182/api/hoteltemps";
 
 let thing: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+
+interface ITemperature
+{
+    HotelID: number;
+    DatoTid: Date;
+    Temperature: number;
+}
 
 axios.get(uri)
     .then(function (response: AxiosResponse): void {
         thing.innerHTML = JSON.stringify(response.data);
     })
     .catch(function (error: AxiosError): void {
-        thing.innerHTML = "hej" + error.message + error.name;
+        thing.innerHTML = "hej" + error.message;
     });
