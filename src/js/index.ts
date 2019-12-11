0ef83d65197c68231ff7c8abc36ce6b3664c0444
CBoens <43374935+CBoens@users.noreply.github.com>
2019-12-11 09:42:44 +0100

    tabelvisning af hoteltemps
    tabelvisning af data fra database.

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,19 +5,29 @@ import axios, {
 
 let uri: string = "http://localhost:50182/api/hoteltemps";
 
+
 let thing: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+let historik: HTMLDivElement = <HTMLDivElement>document.getElementById("historik");
 
 interface ITemperature
 {
-    HotelID: number;
-    DatoTid: Date;
-    Temperature: number;
+    hotelID: number;
+    datoTid: Date;
+    temperature: number;
 }
 
 axios.get(uri)
     .then(function (response: AxiosResponse): void {
         thing.innerHTML = JSON.stringify(response.data);
+        let result: string = "<table>";
+            response.data.forEach((temp: ITemperature) => {
+                result += "<tr><td>" + temp.datoTid + "</td><td>" + temp.temperature + "</td></tr>";
+            });
+            result += "</table>";
+            historik.innerHTML = result;
     })
+
     .catch(function (error: AxiosError): void {
         thing.innerHTML = "hej" + error.message;
     });
+
