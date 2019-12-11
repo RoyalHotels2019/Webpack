863d32352c15295daf48a83f3ff515fb0df49354
thom20m1 <45196128+thom20m1@users.noreply.github.com>
2019-12-11 11:48:46 +0100

    oprettede ts til skrivning af grader

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,7 +6,7 @@ import axios, {
 let uri: string = "http://localhost:50182/api/hoteltemps";
 
 
-let thing: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+let OurTempOutput: HTMLHeadElement = <HTMLHeadElement>document.getElementById("OurTempOutput");
 let historik: HTMLDivElement = <HTMLDivElement>document.getElementById("historik");
 
 interface ITemperature
@@ -15,10 +15,21 @@ interface ITemperature
     datoTid: Date;
     temperature: number;
 }
+axios.get(uri+"/recent")
+    .then(function (response: AxiosResponse): void {
+        let result: string;
+        result = response.data((temp: ITemperature) => {
+            temp.temperature;
+        });
+        result += "°";
+        OurTempOutput.innerHTML = result;
+    })
+    .catch(function (error: AxiosError): void {
+        OurTempOutput.innerHTML = error.message;
+    });
 
 axios.get(uri)
     .then(function (response: AxiosResponse): void {
-        thing.innerHTML = JSON.stringify(response.data);
         let result: string = "<table>";
             response.data.forEach((temp: ITemperature) => {
                 result += "<tr><td>" + temp.datoTid + "</td><td>" + temp.temperature + "</td></tr>";
@@ -28,6 +39,6 @@ axios.get(uri)
     })
 
     .catch(function (error: AxiosError): void {
-        thing.innerHTML = "hej" + error.message;
+        historik.innerHTML = "hej" + error.message;
     });
 
